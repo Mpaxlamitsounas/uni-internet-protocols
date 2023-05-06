c8cf97b6f7c0825ebc7faf8701d431f7d71f3788
Mpaxlamitsounas <worldstudy123@gmail.com>
2023-05-06 19:44:45 +0300

    Add lab 3 exercise

---
+++ lab3/client.py
@@ -0,0 +1,97 @@
+import socket
+
+def start_client():
+    # Επιχειρεί να συνδεθεί στον server
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(("localhost", 12000))
+    except:
+        print("Could not create socket or connect to host")
+        exit()
+        
+    return client_socket
+
+# Διαβάζει τον σωστό αριθμό argument από τον χρήστη
+def read_arguments(function):
+    num_of_args = 0
+    match function:
+        case "+":
+            num_of_args = 4
+        case "*":
+            num_of_args = 3
+        case "-" | "/" | "%":
+            num_of_args = 2
+
+    print("Enter {} arguments, one by one".format(num_of_args))
+    arguments = []
+    for _ in range(num_of_args):
+        arguments.append(input())
+    
+    return arguments
+
+def demo():
+    functions = ["+", "-", "*", "/", "%"]
+    for function in functions:
+        print("Function: " + function)
+        arguments = read_arguments(function)
+                
+        request = function + " " + " ".join(str(argument) for argument in arguments)
+       
+        client_socket = start_client()
+        try:
+            client_socket.send(request.encode("UTF-8"))
+        except:
+            print("Could not send request to host")
+        
+        try:
+            response = client_socket.recv(256).decode("UTF-8")
+            if len(response) > 0:
+                print("Response: " + response)
+            else:
+                print("Did not receive response")    
+        except:
+            print("Did not receive response")
+            
+        client_socket.close()
+        print()
+    
+    
+enable_demo = True
+# demo για τις 5 λειτουργίες με arguments από το πληκτρολόγιο
+if enable_demo:
+    demo()
+    function = "x"
+else:
+    # Τρέχει μέχρι ο χρήστης να δώσει μη valid function 
+    function = input("Enter function (+, -, *, /, %), anything else to exit\n? ")
+    while any([function == "+", function == "-", function == "*", function == "/", function == "%"]):
+        #Διαβάζει τα arguments απ'τον χρήστη, δεν γίνεται κάποιος έλεγχος ορθότητας
+        arguments = read_arguments(function)
+                
+        request = function + " " + " ".join(str(argument) for argument in arguments)
+       
+        # Ανοίγει σύνδεση αφού έχει λάβει τα στοιχεία από τον χρήστη για να μην αφήνει στον server σε αναμονή
+        client_socket = start_client()
+        try:
+            client_socket.send(request.encode("UTF-8"))
+        except:
+            print("Could not send request to host")
+        
+        try:
+            response = client_socket.recv(256).decode("UTF-8")
+            print(response)
+            # Αν λάβει κενή απάντηση, δεν την εμφανίζει
+            if len(response) > 0:
+                print("Response: " + response)
+            else:
+                print("Did not receive response")    
+        except:
+            print("Did not receive response")
+            
+        client_socket.close()
+        print()
+        
+        function = input("Enter function (+, -, *, /, %), anything else to exit\n? ")
+       
+        
+print("Received " + function + " as input, exiting")   
---
+++ lab3/server.py
@@ -0,0 +1,154 @@
+import socket
+import _thread
+
+def start_server():
+     # Επιχειρεί να αρχίσει τον server
+     try:
+          server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          server_socket.bind(("", 12000))
+          server_socket.listen(10)
+          print("The server is ready to receive")
+          # Αν αποτύχει βγάζει μήνυμα και τερματίζει το πρόγραμμα
+     except:
+          print("Failed to start server")
+          exit()
+     
+     # Αλλιώς επιστρέφει το socket στο οποίο τρέχει
+     return server_socket
+     
+def parse(request):
+     # Χωρίζει το request στο πρώτο κενό
+     function, arguments = request.split(" ", 1)
+     # Χωρίζει το δεξί μέρος που έχει τα arguments σε αντικείμενα λίστας
+     arguments = arguments.split()
+     
+     args_not_digit = []
+     parsed_arguments = []
+     for argument in arguments:
+          # Ελέγχει ποιά από αυτά είναι ακέραιοι αριθμοί
+          try:
+               parsed_arguments.append(int(argument))
+          except:
+               args_not_digit.append(argument)
+     
+     # Αν βρεί arguments που δεν μπορεί να μετατρέψει, επιστρέφει ότι μπόρεσε να βρει, και ένα error message
+     if len(args_not_digit) > 0:
+          return False, function, parsed_arguments, args_not_digit, "Argument(s) " + ", ".join(args_not_digit) + " could not be parsed"
+     
+     # Αλλιώς επιστρέφει το function και αριθμούς arguments
+     return True, function, parsed_arguments, [], ""
+
+def validate_args(function, arguments, num_arguments):
+     # Ο ορισμός του valid αλλάζει ανα function
+     allowed_range = range(0, 60000)
+     match function:
+          case "+":
+               valid_num_args = 4
+     
+          case "*":
+               valid_num_args = 3
+                         
+          case "-" | "/" | "%":
+              valid_num_args = 2
+              if function == "-":
+                   allowed_range = range(0, 30000)
+                   
+          # Αν δεν είναι κανένα από τα παραπάνω, δεν είναι ορισμένo function
+          case _:
+               return False, "\tUnrecognised function \"" + function + "\"\n"
+          
+     # Ελέγχει αν ο αριθμός arguments είναι σωστός για το function
+     if num_arguments != valid_num_args:
+          return False, "Incorrect number of number arguments, must be " + str(valid_num_args)
+     
+     # Ελέγχει ποιά αριθμοί arguments είναι εκτός πεδίου ορισμού της πράξης
+     args_out_of_range = []
+     for argument in arguments:
+          if argument not in allowed_range:
+               args_out_of_range.append(str(argument))
+     
+     # Αν βρεθούν παραβιάσεις, επιστρέφει ποιά ήταν εκτός ορίων
+     if len(args_out_of_range) > 0: 
+          return False, "Argument(s) {} out of allowed range of [{}, {}]".format(", ".join(args_out_of_range), allowed_range.start, allowed_range.stop)
+
+     # Δεν γίνεται να επιστραφεί σφάλμα για παραπάνω από 1 error διαφορετικού είδους
+     
+     return True, ""
+
+def calculate(request, function, arguments, panic):
+     result = "In \"" + request + "\":\n"
+     match function:
+          case "+":
+               result = sum(arguments)
+                    
+          case "-":
+               result = arguments[0] - arguments[1]
+                    
+          case "*":
+               result = arguments[0] * arguments[1] * arguments[2]
+                         
+          case "/":
+               if arguments[1] == 0: 
+                    result += "\tCannot divide by 0\n"
+               elif not panic:
+                    result = arguments[0] / arguments[1]
+                    
+          case "%":
+               if arguments[1] == 0: 
+                    result += "\tModulo of 0 is undefined\n"
+               elif not panic:
+                    result = arguments[0] % arguments[1]
+                     
+     return result
+
+def threaded_client(connection_socker, addr):
+     try:
+          request = connection_socket.recv(512).decode("UTF-8")
+     except:
+          print("Failed to connect with client")
+          return
+     
+     result = "In \"" + request + "\":\n"
+     # panic mode == Αν βρεθεί λάθος, ο server θα κάνει όλα τα checks κανονικά αλλά όχι τελικούς υπολογισμούς 
+     # ώστε να δώσει στον χρήστη όσο περισσότερα λάθη που μπορεί να έχει κάνει στο request με το response
+     panic = False
+     
+     # Αν δεχθεί άδειο request, ο server crashάρει
+     if len(request) > 0:
+          valid, function, arguments, invalid_arguemnts, error_message = parse(request)
+          num_arguments = len(arguments) + len(invalid_arguemnts)
+          if not valid:
+               panic = True
+               result += "\t" + error_message + "\n"
+          
+          valid, error_message = validate_args(function, arguments, num_arguments)
+          if not valid:
+               panic = True
+               result += "\t" + error_message + "\n"
+               
+          if not panic:
+               result = calculate(request, function, arguments, panic)
+          
+          # Εμφανίζει το request και αποτέλεσμα στο τέλος
+          print(request + " : " + str(result))
+          try:
+               connection_socket.send(str(result).encode("UTF-8"))
+               connection_socket.close()
+          except:
+               print("Failed to send response to client")
+     else:
+          print("Received empty request")
+
+
+server_socket = start_server()
+
+while True:
+     # Ο server είναι σε αναμονή μέχει να λάβει σύνδεση και δεδομένα
+     try:
+          connection_socket, addr = server_socket.accept()
+          _thread.start_new_thread(threaded_client, (connection_socket, addr))
+          # Αν η σύνδεση αποτύχει, το αγνοεί και μπαίνει ξανά σε αναμονή
+     except:
+          print("Failed to connect with client")
+          continue
+     
